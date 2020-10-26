ffb63afdcb0697b2584ed1b26942731374588995
Enygma <57713995+Aswin-A-Arun@users.noreply.github.com>
2020-10-26 11:24:04 +0530

    Add files via upload

---
+++ swappingFile.py
@@ -0,0 +1,12 @@
+data_a = input()
+data_b = input()
+
+def swapFiledata('data_a','data_b')
+data_a = data_b
+return
+
+with open(sample1.txt,'r')
+data_a = data_b.read()
+
+with open(sample2,'r')
+data_b = data_a.read()
